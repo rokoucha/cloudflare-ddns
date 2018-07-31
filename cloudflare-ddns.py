c13f77c853edfc20fda39fb6107773cc55d24812
Atnanasi <ys2000pro@gmail.com>
2018-07-31 17:20:56 +0900

    Use ident.me

--- cloudflare-ddns.py
+++ cloudflare-ddns.py
@@ -2,24 +2,8 @@ from CloudFlare import CloudFlare
 from os import environ, uname
 from requests import exceptions, get
 
-def main():
-    zone_name = environ["CF_ZONE_NAME"] 
-    dns_name = '{}.{}'.format(uname()[1], environ["CF_DDNS_SUBDOMAIN"] )
-
+def post_record(zone_name, dns_name, target_type, ip_address):
     cf = CloudFlare()
-
-    # Get IP Address
-    target_types = []
-    try:
-        ipv4_address = get('https://v4.ifconfig.co/json').json()['ip']
-        target_types.append('A')
-    except exceptions.RequestException:
-        ipv4_address = ''
-    try:
-        ipv6_address = get('https://v6.ifconfig.co/json').json()['ip']
-        target_types.append('AAAA')
-    except exceptions.RequestException:
-        ipv6_address = ''
 
     # Get zone
     zone_info = cf.zones.get(params={'name': zone_name})
@@ -29,37 +13,67 @@ def main():
         else:
             raise ValueError('Zone is not found')
 
-    # Update v4 record
-    for target_type in target_types:
-        ip_address = ipv4_address if target_type is 'A' else ipv6_address
-        dns_records = cf.zones.dns_records.get(zone_id, params={'name': dns_name + '.' + zone_name, 'type': target_type})
+    # Get record
+    dns_records = cf.zones.dns_records.get(zone_id, params={'name': dns_name + '.' + zone_name, 'type': target_type})
 
-        if len(dns_records) > 0:
-            dns_record = dns_records[0]
-            if 'id' in dns_record:
-                record_id = dns_record['id']
-                record_ip = dns_record['content']
+    # Is record exist?
+    if len(dns_records) > 0:
+        # Update
+        dns_record = dns_records[0]
+        if 'id' in dns_record:
+            record_id = dns_record['id']
+            record_ip = dns_record['content']
 
-                if ip_address==record_ip:
-                    print('UNCHANGED: %s %s %s' % (dns_name, target_type, ip_address))
-                    continue
+            if ip_address==record_ip:
+                print('UNCHANGED: %s %s %s' % (dns_name, target_type, ip_address))
+                return
 
-                cf.zones.dns_records.put(zone_id, record_id, data={
-                    'name': dns_name,
-                    'type': target_type,
-                    'content': ip_address
-                })
-
-                print('UPDATED: %s %s %s -> %s' % (dns_name, target_type, record_ip, ip_address))
-        else:
-            cf.zones.dns_records.post(zone_id, data={
+            # {hostname}.CF_DDNS_SUBDOMAIN.CF_ZONE_NAME(A and AAAA)
+            cf.zones.dns_records.put(zone_id, record_id, data={
                 'name': dns_name,
                 'type': target_type,
                 'content': ip_address
             })
 
-            print('CREATED: %s %s %s' % (dns_name, target_type, ip_address))
-            
+            print('UPDATED: %s %s %s -> %s' % (dns_name, target_type, record_ip, ip_address))
+    else:
+        # Create
+        cf.zones.dns_records.post(zone_id, data={
+            'name': dns_name,
+            'type': target_type,
+            'content': ip_address
+        })
+
+        print('CREATED: %s %s %s' % (dns_name, target_type, ip_address))
+
+
+def main():
+    zone_name = environ["CF_ZONE_NAME"] 
+    dns_name = '{}.{}'.format(uname()[1], environ["CF_DDNS_SUBDOMAIN"] )
+
+    # Get IP Address
+    target_types = []
+    try:
+        ipv4_address = get('https://v4.ident.me').text
+        target_types.append('A')
+    except exceptions.RequestException:
+        ipv4_address = ''
+    try:
+        ipv6_address = get('https://v6.ident.me').text
+        target_types.append('AAAA')
+    except exceptions.RequestException:
+        ipv6_address = ''
+
+    # Update record
+    for target_type in target_types:
+        ip_address = ipv4_address if target_type is 'A' else ipv6_address
+        ip_version = 'v4' if target_type is 'A' else 'v6'
+
+        # A and AAAA record
+        post_record(zone_name, dns_name, target_type, ip_address)
+
+        # A or AAAA only record
+        post_record(zone_name, '{}.{}'.format(ip_version,dns_name), target_type, ip_address)
 
 if __name__ == '__main__':
     main()
